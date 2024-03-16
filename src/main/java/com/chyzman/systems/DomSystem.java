828b779768b1e5c2c455a051dfaf4058a9eb5360
Dragon-Seeker <timekeeperguild@gmail.com>
2024-03-16 17:57:33 -0500

    Add DomSystem method for passing deltatime supplier

--- src/main/java/com/chyzman/systems/DomSystem.java
+++ src/main/java/com/chyzman/systems/DomSystem.java
@@ -2,7 +2,9 @@ package com.chyzman.systems;
 
 import dev.dominion.ecs.api.Dominion;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public interface DomSystem extends Runnable {
 
@@ -25,6 +27,25 @@ public interface DomSystem extends Runnable {
             @Override
             public void run() {
                 consumer.accept(dominion());
+            }
+        };
+    }
+
+    static DomSystem create(Dominion dominion, String name, Supplier<Double> deltaTime, BiConsumer<Dominion, Double> consumer){
+        return new DomSystem() {
+            @Override
+            public Dominion dominion() {
+                return dominion;
+            }
+
+            @Override
+            public String name() {
+                return name;
+            }
+
+            @Override
+            public void run() {
+                consumer.accept(dominion(), deltaTime.get());
             }
         };
     }
