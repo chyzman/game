d7eeb2467179a1cd055bece27e0fd26ecba61225
chyzman <32403637+chyzman@users.noreply.github.com>
2024-03-16 18:23:23 -0400

    added comments to physics system and commented out debug logging

--- src/main/java/com/chyzman/systems/Physics.java
+++ src/main/java/com/chyzman/systems/Physics.java
@@ -12,6 +12,7 @@ public class Physics {
     public static final double TERMINAL_VELOCITY = 100d/(double) LOGIC_TICK_RATE;
 
     public static void update(Dominion dom) {
+        // update velocity of things with gravity
         dom.findEntitiesWith(Velocity.class, Gravity.class).forEach(result -> {
             Velocity velocity = result.comp1();
             Gravity gravity = result.comp2();
@@ -21,11 +22,12 @@ public class Physics {
             }
         });
 
+        // update position of things with velocity
         dom.findEntitiesWith(Position.class, Velocity.class).forEach(result -> {
             Position pos = result.comp1();
             Velocity velocity = result.comp2();
             pos.add(velocity);
-            System.out.printf("\r%s: Pos:(%s, %s, %s) Vel:(%s,%s,%s)", result.entity().getName(), pos.x, pos.y, pos.z, velocity.x*(double)LOGIC_TICK_RATE, velocity.y*(double)LOGIC_TICK_RATE, velocity.z*(double)LOGIC_TICK_RATE);
+//            System.out.printf("\r%s: Pos:(%s, %s, %s) Vel:(%s,%s,%s)", result.entity().getName(), pos.x, pos.y, pos.z, velocity.x*(double)LOGIC_TICK_RATE, velocity.y*(double)LOGIC_TICK_RATE, velocity.z*(double)LOGIC_TICK_RATE);
         });
     }
 }
