7012cddd3810233f1d02bef4bf735b752146f899
chyzman <32403637+chyzman@users.noreply.github.com>
2024-03-19 15:26:48 -0400

    help idk how physics

--- src/main/java/com/chyzman/component/fisics/PhysicsObject.java
+++ src/main/java/com/chyzman/component/fisics/PhysicsObject.java
@@ -4,7 +4,7 @@ import org.ode4j.ode.internal.DxBody;
 import org.ode4j.ode.internal.DxWorld;
 
 public class PhysicsObject extends DxBody {
-    protected PhysicsObject(DxWorld w) {
+    public PhysicsObject(DxWorld w) {
         super(w);
     }
 }
